17ca45eb7604064849c44fe8a8fc17a8d90efd6e
Altieres Lopes <altiereslopes@gmail.com>
2022-01-20 16:19:02 -0300

    starts using typescript

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const axios = require('axios')
-const express = require('express')
+import axios from 'axios'
+import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 const priceCacheTimeInMilis = 5000
-var priceCached = []
+var priceCached: any = []
 var priceCacheLastUpdateTime = 0
 
 async function priceUpdateIfNecessary() {
@@ -15,24 +15,24 @@ async function priceUpdateIfNecessary() {
   const priceResponse = await axios.get(priceUrl)
 
   priceCached = Object.fromEntries(
-    priceResponse.data.map(item => [item.symbol, item.price])
+    priceResponse.data.map((item: any) => [item.symbol, item.price])
   )
   console.log('prices cache updated.')
 
   priceCacheLastUpdateTime = new Date().getTime()
 }
 
-async function priceHandler(req, res) {
+async function priceHandler(req: any, res: any) {
   try {
     const symbolUSDT = req.query.symbol.toUpperCase() + 'USDT'
     const symbolBTC = req.query.symbol.toUpperCase() + 'BTC'
     const symbolBTCUSDT = 'BTCUSDT'
-  
+
     await priceUpdateIfNecessary()
     console.log(`requested prices for ${symbolUSDT}`)
-  
+
     if (priceCached[symbolUSDT]) return res.send(priceCached[symbolUSDT].toString())
-  
+
     res.send((priceCached[symbolBTC] * priceCached[symbolBTCUSDT]).toString())
   } catch (e) {
     res.status(400).send('invalid symbol')
@@ -40,7 +40,7 @@ async function priceHandler(req, res) {
 }
 
 const change24hCacheTimeInMilis = 30000
-var change24hCached = []
+var change24hCached: any = []
 var change24hCacheLastUpdateTime = 0
 
 async function change24hUpdateIfNecessary() {
@@ -50,24 +50,24 @@ async function change24hUpdateIfNecessary() {
   const change24hResponse = await axios.get(change24hUrl)
 
   change24hCached = Object.fromEntries(
-    change24hResponse.data.map(item => [item.symbol, item.priceChangePercent])
+    change24hResponse.data.map((item: any) => [item.symbol, item.priceChangePercent])
   )
   console.log('change24h cache updated.')
 
   change24hCacheLastUpdateTime = new Date().getTime()
 }
 
-async function change24hHandler(req, res) {
+async function change24hHandler(req: any, res: any) {
   try {
     const symbolUSDT = req.query.symbol.toUpperCase() + 'USDT'
     const symbolBTC = req.query.symbol.toUpperCase() + 'BTC'
     const symbolBTCUSDT = 'BTCUSDT'
-  
+
     await change24hUpdateIfNecessary()
     console.log(`requested change24h for ${symbolUSDT}`)
-  
+
     if (change24hCached[symbolUSDT]) return res.send(change24hCached[symbolUSDT].toString())
-  
+
     res.send((parseFloat(change24hCached[symbolBTC]) + parseFloat(change24hCached[symbolBTCUSDT])).toString())
   } catch (e) {
     res.status(400).send('invalid symbol')
